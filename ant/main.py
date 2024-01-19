a5bfe2440d6eb0b9365bb7551c8edf5329e5cf92
Alex Pinkney <apinkney97@gmail.com>
2024-01-19 12:49:02 +0000

    Basic CLI using Typer

--- ant/main.py
+++ ant/main.py
@@ -1,5 +1,9 @@
 import time
 from itertools import count
+from enum import StrEnum, auto
+
+import typer
+from graphics import tk  # type: ignore
 
 from ant.ant import Ant
 from ant.display import Display
@@ -12,6 +16,12 @@ from ant.grid import (
     TriangleGrid,
 )
 from ant.types import AntState, Rule
+
+
+class GridType(StrEnum):
+    SQUARE = auto()
+    HEX = auto()
+    TRIANGLE = auto()
 
 
 def make_ant(
@@ -34,33 +44,6 @@ def make_ant(
     )
 
 
-def lr_square_two() -> list[Ant]:
-    grid = SquareGrid(store_default=True)
-    ants = [
-        make_ant(rules="LR", grid=grid),
-        make_ant(rules="LR", grid=grid),
-    ]
-    return ants
-
-
-def lr_square(rules: str = "LR") -> list[Ant]:
-    return [make_ant(rules=rules)]
-
-
-def lr_tri(rules: str = "LR") -> list[Ant]:
-    return [make_ant(grid=TriangleGrid(store_default=True), rules=rules)]
-
-
-def lr_hex(rules: str = "LR") -> list[Ant]:
-    return [
-        make_ant(
-            grid=HexGrid(store_default=True),
-            rules=rules,
-            start_direction=CardinalDirection.NORTH_WEST,
-        )
-    ]
-
-
 def validate_ants(ants: list[Ant]) -> Grid:
     # Validates all ants live on the same grid
     grid = ants[0].grid
@@ -71,12 +54,12 @@ def validate_ants(ants: list[Ant]) -> Grid:
     return grid
 
 
-def run_live(
+def _run_live(
     ants: list[Ant],
-    step_limit: int = 0,
-    sleep_interval: float = 0.0,
-    steps_per_redraw: int = 1,
-    size_limit: int = 1000,
+    step_limit: int,
+    sleep_interval: float,
+    steps_per_redraw: int,
+    size_limit: int,
 ) -> None:
     if not ants:
         return
@@ -85,32 +68,46 @@ def run_live(
 
     display = Display(grid)
 
+    print("Use Ctrl-C to halt execution")
+
+    wait = True
+
     range_ = range(step_limit) if step_limit else count()
+    i = 0
     for i in range_:
         try:
             for ant in ants:
                 ant.step()
             if i % steps_per_redraw == 0:
                 display.render()
-                display.set_title(f"Ant: {i+1}")
+                display.set_title(f"Ant: {i}")
                 if sleep_interval:
                     time.sleep(sleep_interval)
                 if size_limit:
                     xmin, ymin, xmax, ymax = grid.bbox
                     if xmax - xmin > size_limit or ymax - ymin > size_limit:
+                        print(f"Exceeded maximum size {size_limit} after {i} steps")
                         break
         except KeyboardInterrupt:
             display.render()  # In case we interrupted it
+            display.set_title(f"Ant: {i}")
             print()
             break
+        except tk.TclError:
+            # Handle the window being closed
+            wait = False
+            break
+    else:
+        print(f"Ran for {i} steps")
 
-    input("Hit enter to continue")
+    if wait:
+        input("Hit enter to exit")
 
 
-def run(
+def _run(
     ants: list[Ant],
-    step_limit: int = 1_000_000,
-    size_limit: int = 2000,
+    step_limit: int,
+    size_limit: int,
     title: str = "",
 ):
     grid = validate_ants(ants)
@@ -126,20 +123,54 @@ def run(
     # TODO: Dump as PNG
 
 
+app = typer.Typer()
+
+
+@app.command()
+def run(
+    lr_rules: list[str],
+    grid: GridType = GridType.SQUARE,
+    steps_per_redraw: int = 10_000,
+    sleep_interval: float = 0.0,
+    step_limit: int = 1_000_000,
+    size_limit: int = 1_000,
+):
+    grid_: Grid
+
+    match grid:
+        case GridType.SQUARE:
+            grid_ = SquareGrid()
+        case GridType.HEX:
+            grid_ = HexGrid()
+        case GridType.TRIANGLE:
+            grid_ = TriangleGrid()
+        case _:
+            raise ValueError(f"Unhandled grid type {grid}")
+
+    start_direction = CardinalDirection.NORTH
+    if grid is GridType.HEX:
+        start_direction = CardinalDirection.NORTH_WEST
+
+    ants = []
+    for rules in lr_rules:
+        ants.append(make_ant(rules, grid_, start_direction=start_direction))
+
+    _run_live(
+        ants=ants,
+        steps_per_redraw=steps_per_redraw,
+        sleep_interval=sleep_interval,
+        step_limit=step_limit,
+        size_limit=size_limit,
+    )
+
+
+@app.command()
+def dump():
+    pass
+
+
 def main():
-    limit = jump = pause = 0
-
-    jump = 1000
-    # pause = 0.2
-    # limit = 13000
-
-    # ants = lr_square()
-    # ants = lr_square_two()
-    ants = lr_hex("LR")
-    # ants = lr_tri()
-
-    run_live(ants=ants, steps_per_redraw=jump, sleep_interval=pause, step_limit=limit)
-    # run(ants=ants, step_limit=50, title="something")
+    app()
 
 
 if __name__ == "__main__":
